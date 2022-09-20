585168ddc0def70385ac49cebe91acc69754e31c
jsi2jsi <40815148+jsi2jsi@users.noreply.github.com>
2022-09-20 10:50:47 -0500

    Add files via upload

---
+++ docs/Partc-foros/Partc-foros_resources/js/ardoraSopaCFG.js
@@ -0,0 +1,37 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=0
+var successes=0; successesMax=1; attempts=1; attemptsMax=1;
+var tiTime=false;
+var tiTimeType=0;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var startTime; tiSuccesses=false; tiAttempts=false; tiScore=false;
+typeGame=1;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=4; messageOk="Muy bien. No olvides estas pautas al colaborar en foros"; messageTime=""; isShowMessage=false;
+var urlOk=""; urlTime="";
+var goURLOk="_blank"; goURLTime="_blank"; 
+var borderOk="#008000"; borderTime="#FF0000"; borderAttempts="#FF0000";borderError="#FF0000";
+var imaSel=0; texSel=0;
+var board=[]; var xAnswer=5; answer=[]; rows=6; cols=10; xKey=-1; yKey=-1; animationX=[]; animationY=[]; animationPosX=[]; animationPosY=[]; animationLetter=[];
+var words=["Q09OU0lHTkE=","REVTVklBUlNF","SU5EQUdBTkRP","Q09SVEVTw41B","TEVFUg==","UkVTUEVUTw==","U1VTVEFOQ0lBTA==","UkVQSVRBTg==","UkVGTEVYSU9ORVM=","UkVGRVJFTkNJQVM=","UFVCTElDQVI=","RElBUklBTUVOVEU="];
+var c=[8,9,9,8,4,7,10,7,11,11,8,11];
+var defs=["1. Leer la _____","2. Participar con el tema indicado (No _____ del tema)","3. Sustentar los aportes _____ respecto al tema","4. Ejercita tu _____","5. ____ las participaciones de compañeros para dialogar","6. Dirígete con ______","7. Aportar información ______ del tema","8. Que las aportaciones no se _____","9. De lo indagado, aportar nuestras _____","10. Incorporar las ______","11. No _____ contenido personal","12. Ingresa y colabora _______"];
+var wDefs=350; posCheck=[];
+var answers=[]; letters=[]; indexLetters=0; xSel=-1; ySel=-1; iLetter=-1; jLetter=-1;
+var wordsGame="UGFydGMtZm9yb3M="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var numCols=11; numRows=11; sizeCell=20;
+var direction=["HD","VD","X2I"];
+var boardGame=[[".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".","."]];
+var solved1X=[];solved1Y=[];solved2X=[];solved2Y=[];paintSolved=["N","N","N","N","N","N","N","N","N","N","N","N"];x1=[];y1=[];x2=[];y2=[];
+var fillLetter="ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚÜ";
